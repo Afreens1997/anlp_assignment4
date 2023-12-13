d68376fe38b65e61fb18faabdf68417451c32297
afreens <afreens@andrew.cmu.edu>
2023-12-13 03:33:03 -0500

    adding baseline and eval code

--- baseline.py
+++ baseline.py
@@ -15,16 +15,16 @@ option_map = {
 def get_baseline_answer(tokenizer, model, prompt, dp):
     article = dp["article"]
     summary = dp["question"]
-    complete_prompt = f"{prompt} \nArticle: {article} \nSummary: {summary} \n@placeholder options {get_text_with_option_words(dp)}"
+    complete_prompt = f"{prompt} \nArticle: {article} \nSummary: {summary} \n@placeholder options {get_text_with_option_words(dp)} \n @placeholder replacement:"
     # print(complete_prompt)
     inputs = tokenizer(complete_prompt, return_tensors="pt", max_length=2048)
-    outputs = model.generate(**inputs, max_length=3)
+    outputs = model.generate(**inputs, max_length=5)
     return tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
 
 def baseline_generation():
     # train_dataset = load_jsonl("./dataset/train.jsonl")
     # test_dataset = load_jsonl("./dataset/test.jsonl")
-    dev_dataset = load_jsonl("./dataset/baseline/dev.jsonl")
+    dev_dataset = load_jsonl("./data/baseline/dev_instruction_large.jsonl")
 
     sample_article = 'Notices of intended prosecution claimed 991 drivers had broken a 40mph (64km/h) speed limit in Conwy tunnel last October. But the limit had been imposed for night maintenance only and not lifted in the morning as it should have been. Within days the drivers got an apology in the post. North Wales Police released the figures in a Freedom of Information reply. The force said: \"The issue was caused by an administration error surrounding the enforcement period. \"North Wales police do not record the cost of cancelling notices.\"'
     sample_summary = "Nearly 1,000 drivers were wrongly sent speeding notices after a @placeholder limit on a north Wales road was not lifted , figures have shown ."
@@ -36,33 +36,33 @@ def baseline_generation():
     sample_placeholder = "secret"
     example2 = f'Article: {sample_article} \nSummary: {sample_summary} \n@placeholder replacement: {sample_placeholder}'
 
-    prompt_basic = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word."
-    prompt_with_task_description = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts."
-    prompt_1_shot = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts. For example: \n{example1}"
+    prompt_basic = f"Fill in the @placeholder with one word from the given options which best fits the @placeholder in the summary of the article?"
     prompt_2_shot = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts. For example: \n{example1} \n{example2}\n"
+    # prompt_with_task_description = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts."
+    # prompt_1_shot = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts. For example: \n{example1}"
+    # prompt_2_shot = f"The task is to fill in the @placeholder in the summary. Based on the article provided, fill in the @placeholder in the summary one of the best suited option word. Each option is an abstract word. Abstract words refer to intangible qualities, ideas, and concepts. For example: \n{example1} \n{example2}\n"
 
-    dev_dataset.reverse()
     for i, dp in enumerate(dev_dataset):
-        if "baseline_answers" in dp:
-            continue
+        # if "baseline_answers" in dp:
+        #     continue
         
         dp["baseline_answers"] = {}
         
         answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_basic, dp)
         dp["baseline_answers"]["prompt_basic"] = answer
 
-        answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_with_task_description, dp)
-        dp["baseline_answers"]["prompt_with_task_description"] = answer
+        # answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_with_task_description, dp)
+        # dp["baseline_answers"]["prompt_with_task_description"] = answer
 
-        answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_1_shot, dp)
-        dp["baseline_answers"]["prompt_1_shot"] = answer
+        # answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_1_shot, dp)
+        # dp["baseline_answers"]["prompt_1_shot"] = answer
 
-        answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_2_shot, dp)
-        dp["baseline_answers"]["prompt_2_shot"] = answer
+        # answer = get_baseline_answer(t5_tokenizer, t5_mlm, prompt_2_shot, dp)
+        # dp["baseline_answers"]["prompt_2_shot"] = answer
         print(i, dp["baseline_answers"], option_map[dp["label"]])
         if i%50 == 0:
-            write_jsonl("./dataset/baseline/dev_reverse.jsonl", dev_dataset)
+            write_jsonl("./data/baseline/dev_instruction_large.jsonl", dev_dataset)
 
-    write_jsonl("./dataset/baseline/dev_reverse.jsonl", dev_dataset)
+    write_jsonl("./data/baseline/dev_instruction_large.jsonl", dev_dataset)
 
 baseline_generation()
--- evaluate.py
+++ evaluate.py
@@ -18,7 +18,7 @@ def accuracy(data, answer_lambda=lambda x:x["answer"]):
         if option_map[dp['label']] == process_options(answer_lambda(dp)):
             correct_count+=1
         # else:
-            # print(option_map[dp['label']], process_options(answer_lambda(dp)))
+        #     print(option_map[dp['label']], process_options(answer_lambda(dp)))
     acc = round(float(correct_count/len(data)), 3)
     return acc
 if __name__ == "__main__":
@@ -26,6 +26,9 @@ if __name__ == "__main__":
     print("prompt_with_task_description", accuracy(load_jsonl("./dataset/baseline/dev.jsonl"), lambda x:x["baseline_answers"]["prompt_with_task_description"]))
     print("prompt_1_shot", accuracy(load_jsonl("./dataset/baseline/dev.jsonl"), lambda x:x["baseline_answers"]["prompt_1_shot"]))
     print("prompt_2_shot", accuracy(load_jsonl("./dataset/baseline/dev.jsonl"), lambda x:x["baseline_answers"]["prompt_2_shot"]))
+    print("prompt_basic", accuracy(load_jsonl("./dataset/baseline/dev_max_length_5.jsonl"), lambda x:x["baseline_answers"]["prompt_basic"]))
+    print("prompt_basic", accuracy(load_jsonl("./dataset/baseline/dev_instruction.jsonl"), lambda x:x["baseline_answers"]["prompt_basic"]))
+    print("prompt_basic", accuracy(load_jsonl("./dataset/baseline/dev_instruction_large.jsonl"), lambda x:x["baseline_answers"]["prompt_basic"]))
 
 
 
